fa9f6cb40e0188542a9aeecdda25b7a30d23ebba
Muhammad Rafi Dhiyaulhaq <rafidhiyaulh@gmail.com>
2025-11-02 20:08:07 +0700

    chore: configure static export for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = Boolean(process.env.NEXT_PUBLIC_GITHUB_PAGES);
+
+const repoName = "myprogress";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isGithubPages ? `/${repoName}/` : undefined,
+  basePath: isGithubPages ? `/${repoName}` : undefined,
+  trailingSlash: true,
 };
 
 export default nextConfig;
